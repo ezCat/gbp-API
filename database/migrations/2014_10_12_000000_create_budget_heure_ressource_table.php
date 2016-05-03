b21ed693f4311822de8cebd99a51610034fc8200
Dennis <dennis_wittmer@yahoo.fr>
2016-05-03 16:20:27 +0200

    Update migration
    Add table budget_heure_ressource

--- database/migrations/2014_10_12_000000_create_budget_heure_ressource_table.php
+++ database/migrations/2014_10_12_000000_create_budget_heure_ressource_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEnsembleTable extends Migration
+class CreateBudgetHeureRessourceTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,11 @@ class CreateEnsembleTable extends Migration
      */
     public function up()
     {
-        Schema::create('emsemble', function (Blueprint $table) {
+        Schema::create('budget_heure_ressource', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('en_libelle');
-            $table->float('en_budget_commande')->default(0);
-            $table->longText('en_commentaire');
             $table->foreign('fk_id_etat')->references('id')->on('etat');
-            $table->foreign('fk_id_projet')->references('id')->on('projet');
+            $table->foreign('fk_id_ensemble')->references('id')->on('ensemble');
+            $table->foreign('fk_id_ressource')->references('id')->on('ressource');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateEnsembleTable extends Migration
      */
     public function down()
     {
-        Schema::drop('emsemble');
+        Schema::drop('budget_heure_ressource');
     }
 }
