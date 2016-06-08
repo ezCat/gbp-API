f3d23e571a4d605c59e76f5a1a3608b97060cf4d
ezCat <dennis_wittmer@yahoo.fr>
2016-06-08 16:26:03 +0200

    Init console log

--- public/js/ajax/getProjet.js
+++ public/js/ajax/getProjet.js
@@ -6,21 +6,27 @@ $(document).ready(function(){
             url: 'http://localhost/gbp-API/public/ajax/getListeProjet',
             dataType: 'json',
             success: function (json) {
-                $('#table-liste-projet').html('<table>');
+                // $('#table-liste-projet').append('<table id="aze">');
+                console.log('<table border="1"><tr></tr>')
                 $.each(json, function(i, val) {
                     $.each(val, function(i, val) {
                         if (i == "id") {
-                            $('#table-liste-projet').append('<tr id="' + val + '">');
+                            console.log('<tr id="projet-id-' + val + '">')
+                            // $('#aze').append('<tr id="projet-id-' + val + '">');
                         } else {
-                            $('#table-liste-projet').append(
-                                '<td><input type="text" class="form-tab" name="' + i + '" value="' + val + '"></td>'
-                            );
+                            console.log('<td>' + val + '</td>')
+                            // $('#projet-id-' + val).append(
+                            //     '<td><input type="text" class="form-tab" name="' + i + '" value="' + val + '"></td>'
+                            // );
+                        }
+                        if ( i == 'p_commentaire') {
+                            console.log('</tr>')
+                            // $('#projet-id-' + val).append('<td style="padding-top: 15px" class="supprimer-click-projet"><i class="fa fa-close fa-2x"></i></td></tr>');
                         }
                     });
-                     $('#table-liste-projet').append('<td style="padding-top: 15px" class="supprimer-click"><i class="fa fa-close fa-2x"></i></td>');
-                    $('#table-liste-projet').append('</tr>');
                 });
-                $('#table-liste-projet').append('</table>');
+                console.log('</table>')
+                // $('#aze').append('</table>');
             },
             error: function () {
                 alert("Erreur dans la récupération des données, contactez l'administrateur.");
