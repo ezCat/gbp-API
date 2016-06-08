ebfb91c0427e6a45500442bd37205b0573cba808
ezCat <dennis_wittmer@yahoo.fr>
2016-06-08 16:22:37 +0200

    Add js ajax deleteProjet deleteRessource

---
+++ public/js/ajax/deleteProjet.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    $('.supprimer-click-projet').on('click', function(){
+
+        // Confirmation
+        var r = confirm("Voulez-vous supprimer cet enregistrement ?");
+        if (r == false) {
+            return false
+        }
+        
+        var id = $(this).parent("tr").attr('id').slice(-1)
+        console.log(id)
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost/gbp-API/public/ajax/deleteProjet',
+            data: {'id': id},
+            success: function (id) {
+                $('#projet-id-' + id).remove();
+            },
+            error: function () {
+                alert("Erreur dans l'enregistrement des données, contactez l'administrateur.")
+            }
+        });
+    });
+});
